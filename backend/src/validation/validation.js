f28aaa1a80b54fd6ad40adf31ffcc9823d35f211
Kevin Neel <87483788+KevinNeel@users.noreply.github.com>
2024-09-04 17:45:59 +0530

    Update validation.js

--- backend/src/validation/validation.js
+++ backend/src/validation/validation.js
@@ -15,11 +15,3 @@ export const validate_Password = [
     body('password').isLength({ min: 6 }).withMessage('Min 6 length characters required'),
     body('newPassword').isLength({ min: 6 }).withMessage('Min 6 length characters required')
 ]
-
-export const validate_Product = [
-    body('prodcut_Name').notEmpty().withMessage('Field cannot be empty'),
-    body('description').notEmpty().withMessage('Field cannot be empty'),
-    body('quantity').isNumeric().withMessage('Only Decimals allowed'),
-    body('price').isNumeric().withMessage('Only Decimals allowed'),
-    body('category').notEmpty().withMessage('Field cannot be empty'),
-]
